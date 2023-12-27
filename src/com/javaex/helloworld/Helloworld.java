6f82b6beb9d55da494872c42ab6b451936ce0885
neulHa33 <moonn3940@gmail.com>
2023-12-27 10:59:17 +0900

    추가

--- src/com/javaex/helloworld/Helloworld.java
+++ src/com/javaex/helloworld/Helloworld.java
@@ -1,8 +1,26 @@
 package com.javaex.helloworld;
 
+import java.util.Scanner;
+
 public class Helloworld {
 	public static void main(String[] args) {
-		System.out.println("HelloWorld");
+		Scanner sc = new Scanner(System.in);
+		System.out.print("근무 시간 :  ");
+		
+		int workTime = sc.nextInt();
+		int salary;
+		
+		if(workTime<9) {
+			salary = workTime * 10000;
+			
+		} else {
+			salary = 8*10000 + (workTime -8)*12000;
+			
+		}
+		
+		System.out.println("임금은 " + salary + "원 입니다.");
+		
+		sc.close();
 	}
 
 }
